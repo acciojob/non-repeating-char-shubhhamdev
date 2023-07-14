7683748239b5194a527bb94925aef287ffa4246c
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-14 20:35:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+function findFirstNonRepeatedChar() {
+  const inputString = prompt("Enter a string:");
+
+  const charCount = {};
+
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+const result = findFirstNonRepeatedChar();
+if (result) {
+  alert(result);
+} else {
+  alert();
+}
